370d122e3c153e611cb7f866902869b8611d7509
ashithilakarathna2003 <ashithilakarathna@gmail.com>
2024-07-29 21:57:17 +0530

    Add files via upload

---
+++ QuickSort/src/quick/QuickSortImplementation.java
@@ -0,0 +1,53 @@
+package quick;
+
+public class QuickSortImplementation {
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high]; 
+        int i = (low - 1); 
+
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1; 
+    }
+
+    public static void printArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] array = {12,16,4,8,11,19};
+
+        System.out.println("Given Array");
+        printArray(array);
+
+        quickSort(array, 0, array.length - 1);
+
+        System.out.println("\nSorted Array");
+        printArray(array);
+    }
+}
